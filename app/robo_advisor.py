0c7ff5278b75595d1777a13e4b23a2c3280318f2
Shana Shin <sshshana@gmail.com>
2021-03-07 03:22:36 +0900

    dynamically get data from alphavantage
    data collection

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -8,17 +8,30 @@ import requests
 import json
 # from getpass import getpass
 
+from datetime import datetime
 from dotenv import load_dotenv 
-
-# After that, we generally run any setup code, like setting environment vars:
 
 load_dotenv() # read env var(API key) from the ".env" file (read README.md)
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="abc123") # uses the os module to read the specified environment variable and store it in a corresponding python variable
+
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+
+    Param: my_price (int or float) like 4000.444444
+
+    Example: to_usd(4000.444444)
+
+    Returns: $4,000.44
+    """
+    return "${0:,.2f}".format(my_price) #f"${my_price:,.2}" #> $12,000.71
 
 
 #
 # info inputs
 #
+
+current_time = datetime.now().strftime("%Y-%m-%d")
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey={API_KEY}"
 response = requests.get(request_url)
@@ -28,8 +41,24 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
-breakpoint()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+tsd = parsed_response["Time Series (Daily)"]
+dates = list(tsd.keys()) # TODO: assumes first day is on top, but consider shorting to ensure latest day is first
+latest_day = dates[0]
+lastest_close = tsd[latest_day]["4. close"]
+
+# get high & low price from each day
+high_prices = []
+low_prices = []
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    high_prices.append(float(high_price))
+    low_price = tsd[date]["3. low"]
+    low_prices.append(float(low_price))
+# maximum of all high prices / minimum of all low prices
+recent_high = max(high_prices)
+recent_low = min(low_prices)
 
 
 
@@ -41,12 +70,12 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT:", current_time) # shopping cart project
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
+print("LATEST DAY:", last_refreshed)
+print("LATEST CLOSE:", to_usd(float(lastest_close)))
+print("RECENT HIGH:", to_usd(float(recent_high)))
+print("RECENT LOW:", to_usd(float(recent_low)))
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
