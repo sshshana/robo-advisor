d5a3a8f85a269380f06d3b6f466c6a51a3877177
Shana Shin <sshshana@gmail.com>
2021-03-09 08:38:06 +0900

    52-week high & low updates

--- README.md
+++ README.md
@@ -34,7 +34,6 @@ pip install -r requirements.txt
 
 
 ## Setup
- 
 Before using this application, you need an API Key to issue requests to the [AlphaVantage API Key](https://www.alphavantage.co/). Please click the link and get your API Key. (e.g. "abc123")
 
 Then, in your project repository, create an .env file and set a variable called `ALPHAADVANTAGE_API_KEY` and assign the value of your API key as a string. Please see the example below:
@@ -55,17 +54,20 @@ Once launched, the application will ask you to enter the stock symbol of your in
 
 > NOTE: Free version of Alpha Vantage has a volume limit of requests: 5 API requests per minute; 500 API requests per day. The application will accept up to five stock inputs at once to prevent any error. In certain use cases that exceed the limit, consider getting [a premium plan](https://www.alphavantage.co/premium/#:~:text=Alpha%20Vantage%20Premium%20API%20Key,-Welcome%20to%20Premium&text=In%20certain%20use%20cases%20that,help%20you%20scale%20your%20application.) Alpha Vantage offers.
 
+Once you enter the symbol, historical data of the past 20+ years will be retrieved, and the application will create a plot of the adjusted close stock prices. All stocks that you choose will be plotted on a single chart.
 
-Once you enter the symbol, historical data of the past 20+ years will be retrieved, and the application will calculate the annualized return based on the data, following the steps below:
- 1. Retrieve closed stock prices of the past 52 weeks.
+If you close the window that shows the chart, the application will give you more detailed information of selected stock(s), including latest adjusted close price, 52-week high, and 52-week low.
+
+> NOTE: In order to mitigate the confusion caused by the stock split, this application regards the maximum and minimum of the stock's adjusted close prices as **52-week high and 52-week low**. 
+
+The application also calculates the annualized return based on the data, following the steps below:
+ 1. Retrieve adjusted close stock prices of the past 52 weeks.
  2. Calculate wekly returns.
  3. Calcaulte geometric average of the weekly returns.
  4. Annualize the calculated geometric-average return.
 
-Based on the annualized return, the application will tell you whether the expected return on the stock is high, medium, or low. It will also provide you the reason for its evaluation.
+Based on the annualized return, the application will tell you whether the expected return on the stock is **high** (return > 30%), **medium** (30% => return > 2%), or **low** (return <=2%). It will also provide you the reason for its evaluation.
 
 The retrieved stock data will be stored as a CSV file under the subdirectory called "data". The CSV file will be named as, for example, "stockdata_AAPL.csv", "stockdata_MSFT.csv",  etc.
 
-Finally, the application will create a plot of the closed stock prices. All stocks that you choose will be plotted on a single chart.
-
-> NOTE: if you see an error like "ModuleNotFoundError: No module named '...'", it's because the given package isn't installed, so run the `pip` command above to ensure that package has been installed into the virtual environment
+> NOTE: If you see an error like "ModuleNotFoundError: No module named '...'", it's because the given package isn't installed, so run the `pip` command above to ensure that package has been installed into the virtual environment
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,8 +1,6 @@
 # this is the "app/robo_advisor.py" file
 
 # Import all the modules and third-party packages that contain the functionality we need:
-# We want to allow users choose their own user names by creating a separate virtual environment
-
 import csv
 import os
 import requests
@@ -27,8 +25,6 @@ today_date = datetime.now().strftime("%Y-%m-%d %I:%M %p")
 load_dotenv() # read env var(API key) from the ".env" file (read README.md)
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY") # uses the os module to read the specified environment variable and store it in a corresponding python variable
 
-
-# preliminary input validation
 while True:
     stock_symbols_st = input("""
         Please input a stock or cryptocurrency symbol.
@@ -37,16 +33,16 @@ while True:
         """)
     stock_symbols = list(stock_symbols_st.split(" "))
 
-    if len(stock_symbols) > 5:
+    if len(stock_symbols) > 5: # limit the number of requests to be processed at once
         print("The application can access only up to FIVE stocks at once. Please try again.")
     else:
         responses = []
         for stock_symbol in stock_symbols:
-            if 1 <= len(stock_symbol) <= 5 and stock_symbol.isdecimal() == False:
-                request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={stock_symbol}&apikey={API_KEY}"
+            if 1 <= len(stock_symbol) <= 5 and stock_symbol.isdecimal() == False: # preliminary input validation
+                request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={stock_symbol}&apikey={API_KEY}"
                 response = requests.get(request_url)
                 
-                if response.status_code != 200 or "Error Message" in response.text:
+                if response.status_code != 200 or "Error Message" in response.text: # further input vaildation (not available)
                     print(f"Sorry, couldn't find any trading data for {stock_symbol}. Please try again.")
                     exit()
                 else:
@@ -67,37 +63,32 @@ for response in responses:
     stock_name = parsed_response["Meta Data"]["2. Symbol"]
     last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-    tsw = parsed_response["Weekly Time Series"]
+    tsw = parsed_response["Weekly Adjusted Time Series"]
     dates = list(tsw.keys())
     latest_day = dates[0]
-    lastest_close = tsw[latest_day]["4. close"] 
+    lastest_close = tsw[latest_day]["5. adjusted close"] 
     
     #
-    # calculate 52-week high and 52-week low
+    # calculate 52-week high and 52-week low:
+    #   52-week high and low are calculated based on the adjusted close prices
+    #     in order to prevent any confusion caused by stock splits
     #
 
-    # get high & low & closed prices from weekly data
-    high_prices = []
-    low_prices = []
-    closed_prices = []
+    # get adjusted close prices from weekly data
+    close_prices = []
 
-    i = 1 # to select the most recent 52 weeks
+    i = 1 # select the most recent 52 weeks
     for date in dates:
         if i > 52:
             break
         else:
-            high_price = tsw[date]["2. high"]
-            high_prices.append(float(high_price))
-            low_price = tsw[date]["3. low"]
-            low_prices.append(float(low_price))
-            closed_price = tsw[date]["4. close"]
-            closed_prices.append(float(closed_price))
+            close_price = tsw[date]["5. adjusted close"]
+            close_prices.append(float(close_price))
             i += 1
 
-    # maximum of all high prices & minimum of all low prices
-    recent_high = max(high_prices)
-    recent_low = min(low_prices)
-
+    # maximum of all adjusted close prices
+    recent_high = max(close_prices)
+    recent_low = min(close_prices)
 
     #
     # info outputs
@@ -105,7 +96,7 @@ for response in responses:
 
     csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"stockdata_{stock_name}.csv")
 
-    csv_headers = ("date", "open", "high", "low", "close", "volume")
+    csv_headers = ("date", "open", "high", "low", "close", "adjusted close", "volume", "dividend amount")
     with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
         writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
         writer.writeheader() # uses fieldnames set above
@@ -119,15 +110,17 @@ for response in responses:
                 "high": float(weekly_data["2. high"]),
                 "low": float(weekly_data["3. low"]),
                 "close": float(weekly_data["4. close"]),
-                "volume": float(weekly_data["5. volume"])
+                "adjusted close": float(weekly_data["5. adjusted close"]),
+                "volume": float(weekly_data["6. volume"]),
+                "dividend amount": float(weekly_data["7. dividend amount"])
                 })
 
 
-    # calculate weekly returns from the closed prices list
+    # calculate weekly returns from the close prices list
     weekly_returns = []
     w = 0
-    for w in range(len(closed_prices)-1):
-        weekly_returns.append(closed_prices[50-w]/closed_prices[51-w]-1)
+    for w in range(len(close_prices)-1):
+        weekly_returns.append(close_prices[50-w]/close_prices[51-w]-1)
 
     # calcualte annualized return (using geometric average approach)
 
@@ -165,11 +158,11 @@ for response in responses:
 
     # store data in dataframe to create a plot at the end of the application
     df = pd.DataFrame.from_dict(tsw, orient="index").iloc[:52]
-    df = df.iloc[::-1] # reverse the order of the data (from oldest to newest)
-    globals()[f"df_{responses.index(response)}"] = df.astype(float)
+    df = df.iloc[::-1] # reverse the order of the data (data start from the oldest)
 
-    # convert data type from string to float
-    # df[["open", "high", "low", "close", "volume"]] = df[["open", "high", "low", "close", "volume"]].astype(float)
+    # convert the datatype to float and store the dataframe in a dynamic dataframe
+    # to prevent from it being overwritten in the following loop
+    globals()[f"df_{responses.index(response)}"] = df.astype(float) 
 
     # print the results
     print(f"""
@@ -195,19 +188,20 @@ for response in responses:
     -------------------------
     """)
 
-# show a plot that has all the stock closed price on one graph
+# create a plot that has all the stock's adjusted close prices (52 weeks) on one graph
 all_df = pd.DataFrame(index=df_0.index)
 s = 0
 for stock in stock_symbols:
-    all_df[stock] = globals()[f"df_{s}"]["4. close"]
+    all_df[stock] = globals()[f"df_{s}"]["5. adjusted close"]
     s += 1
-
-# plt.figure()
 all_df.plot()
+
+# format the plot
 plt.legend(loc='best')
 plt.title("Stock Prices over 52 Weeks", fontsize=20)
 plt.xlabel("Time")
 plt.ylabel("Prices")
 plt.gca().yaxis.set_major_formatter('${x:,.0f}')
 
+# show the plot
 plt.show()
